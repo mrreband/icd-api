ee776b6d6725c3e57ee0fe7c6d2c1f248e11eb9a
mrreband <mr@reband.org>
2023-04-17 11:56:48 -0500

    adding a setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+install_requires = [
+    'python-dotenv',
+    'requests',
+]
+
+tests_require = [
+    'pytest>=5.1.2',
+]
+
+setup(
+    name='icd-api',
+    version="0.0.1",
+    description='',
+    url='https://github.com/mrreband/icd-api',
+    packages=['icd_api'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    zip_safe=False,
+)
