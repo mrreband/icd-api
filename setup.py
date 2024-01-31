3e9b7d7752c9246b0a61dffe985bd07af2ec709a
mrreband <mr@reband.org>
2024-01-31 11:59:53 -0600

    increment patch version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 
 setup(
     name='icd-api',
-    version="0.0.2",
+    version="0.0.3",
     description='',
     url='https://github.com/mrreband/icd-api',
     packages=['icd_api'],
